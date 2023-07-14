f8675f38573cee0971eba59c01fed63c2f924f82
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-07-14 10:49:12 -0500

    write solutions

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagram_words = []
+        sotred_word = sorted([c for c in self.word])
+        for word in word_list:
+            if sorted([c for c in word]) == sotred_word:
+                anagram_words.append(word)
+        return anagram_words
